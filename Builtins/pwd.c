a76774e59122c2fdc9cf73b26e347cf34ce5a4c6
Cloe-42 <cdenaux@student.42wolfsburg.de>
2026-03-06 17:20:12 +0100

    Partial pwd

--- Builtins/pwd.c
+++ Builtins/pwd.c
@@ -10,6 +10,23 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/* Shows the full path of the folder you're currently in
+subject says no option needed (-L nor -P)
+should display: /home/user/my_directory 
+uses authorized function getcwd()*/
+
 #include "minishell.h"
 
-whatever ft_pwd(whatever);
+int ft_pwd(void)
+{
+    char cwd[4096];
+
+    if (getcwd(cwd, sizeof(cwd)))
+    {
+        printf("%s\n", cwd);
+        return (0);
+    }
+    //print_error("pwd", NULL, strerror(errno)); => needs print_error function
+    //strerror is an authorized function
+    return (1);
+}
