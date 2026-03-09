125726503ecdb3738f7de97afde265b02e402b47
Cloe-42 <cdenaux@student.42wolfsburg.de>
2026-03-09 16:13:24 +0000

    ft_env according to subject

--- Builtins/env.c
+++ Builtins/env.c
@@ -12,4 +12,37 @@
 
 #include "minishell.h"
 
-"whatever" ft_env(whatever);
+
+/*Helper to display the variable as "key=value\n"
+Only displays if there's a value stored
+write() instead of printf() to avoid leaks (possible buffer issue with pipes)*/
+
+static void print_env_var(t_env_var *var)
+{
+    if (!var->value)
+        return;
+    write(STDOUT_FILENO, var->key, ft_strlen(var->key));
+    write(STDOUT_FILENO, "=", 1);
+    write(STDOUT_FILENO, var->value, ft_strlen(var->value));
+    write(STDOUT_FILENO, "\n", 1);
+}
+
+
+/*Spans the linked list and displays each variable
+Return 1 if env is not valid, otherwise 0
+no options or arguments as asked in the subject*/
+
+int ft_env(t_env *env)
+{
+    t_env_var   *current;
+
+    if (!env || !env->vars)
+        return (1);
+    current = env->vars;
+    while (current)
+    {
+        print_env_var(current);
+        current = current->next;
+    }
+    return (0);
+}
