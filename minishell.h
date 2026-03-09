f825d254049f2dd822c9a86baecd801a0e29ce4d
Cloe-42 <cdenaux@student.42wolfsburg.de>
2026-03-09 14:56:13 +0000

    Structures added in the main header

--- minishell.h
+++ minishell.h
@@ -16,7 +16,6 @@
 # define INT_MAX 2147483647
 # define INT_MIN -2147483648
 
-/*Pour utiliser fonctions et structure*/
 # include <fcntl.h>
 # include <stdarg.h>
 # include <stdbool.h>
@@ -29,9 +28,43 @@
 # include <readline/history.h>
 # include "libft.h"
 
+/*Structures*/
+typedef struct s_env_var //One node = one variable
+{
+    char                *key; //"PATH"
+    char                *value; //"/usr/bin:/bin"
+    struct s_env_var    *next;
+}   t_env_var;
+
+typedef struct s_env
+{
+    t_env_var   *vars; //Head of the linked list
+    char        **envp_array; //Array used for execve(), can be NULL initially
+}   t_env;
+
+typedef struct s_cmd //Each t_cmd represents a segment between pipes
+{
+    char            **args; //argv: ["ls", "-la", NULL]
+    t_redir         *redirs; //List of redirections
+    struct s_cmd    *next; //Next command in the pipe
+}   t_cmd;
+
+typedef struct s_expand_ctx
+{
+    t_env   *env; //To solve $VAR
+    int     last_status; //To solve $?
+}   t_expand_ctx;
+
+/*Global variable signal*/
+
+extern int  g_signal_received;
+//Only global allowed. Handles SIGINT and SIGQUIT.
 
 /*In cmd_utils.c*/
 char		*get_path(char **envp);
 char		*get_cmd_path(char *cmd, char **envp);
+
+/*In Builtins*/
+int ft_echo(char **args);
 
 #endif
