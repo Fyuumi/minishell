718d233055b45f2158945274a128a1466c846990
Fyuumi <73714037+Fyuumi@users.noreply.github.com>
2026-04-07 19:22:35 +0000

    Add files via upload

--- minishell.h
+++ minishell.h
@@ -16,7 +16,6 @@
 # define INT_MAX 2147483647
 # define INT_MIN -2147483648
 
-/*Pour utiliser fonctions et structure*/
 # include <fcntl.h>
 # include <stdarg.h>
 # include <stdbool.h>
@@ -25,10 +24,42 @@
 # include <sys/types.h>
 # include <sys/wait.h>
 # include <unistd.h>
+# include <signals.h>
 # include <readline/readline.h>
 # include <readline/history.h>
-# include <signal.h>
 # include "libft.h"
+
+/*Structures*/
+typedef struct s_env_var //One node = one variable
+{
+    char                *key; //"PATH"
+    char                *value; //"/usr/bin:/bin"
+    struct s_env_var    *next;
+}   t_env_var;
+
+typedef struct s_env
+{
+    t_env_var   *vars; //Head of the linked list
+    char        **envp_array; //Array used for execve(), can be NULL initially
+}   t_env;
+
+typedef struct s_cmd //Each t_cmd represents a segment between pipes
+{
+    char            **args; //argv: ["ls", "-la", NULL]
+    t_redir         *redirs; //List of redirections
+    struct s_cmd    *next; //Next command in the pipe
+}   t_cmd;
+
+typedef struct s_expand_ctx
+{
+    t_env   *env; //To solve $VAR
+    int     last_status; //To solve $?
+}   t_expand_ctx;
+
+/*Global variable signal*/
+
+extern int  g_signal_received;
+//Only global allowed. Handles SIGINT and SIGQUIT.
 
 /*In cmd_utils.c*/
 char		*get_path(char **envp);
@@ -36,6 +67,7 @@ char		*get_cmd_path(char *cmd, char **envp);
 
 /*In Builtins*/
 int ft_echo(char **args);
-void sig(void); // in exit.c
+int ft_env(t_env *env);
+void ft_sig(void);
 
 #endif
